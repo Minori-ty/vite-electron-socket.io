a0b4dcb0d231ec20272dad4a03fc29bf8ca95a82
XuZehong <xuzehong@sat-tech.cn>
2022-02-14 11:48:12 +0800

    fix

--- server/app.ts
+++ server/app.ts
@@ -1,26 +1,27 @@
-import { Server } from 'socket.io';
+import { Server } from 'socket.io'
 
-let a = 1;
-const io = new Server({ cors: { origin: '*' } });
+let a = 1
+const io = new Server({ cors: { origin: '*' } })
 
 io.on('connection', (socket: any) => {
-  console.log(`connect ${socket.id}`);
+    console.log(`connect ${socket.id}`)
 
-  //监听disconnect事件
-  socket.on('disconnect', () => {
-    //逻辑处理
-    console.log(`客户端离开页面 ${socket.id}`);
-  });
+    //监听disconnect事件
+    socket.on('disconnect', () => {
+        //逻辑处理
+        console.log(`客户端离开页面 ${socket.id}`)
+    })
 
-  socket.on('send', (data: any) => {
-    console.log('接收到客户端的消息', data);
+    socket.on('send', (data: any) => {
+        console.log('接收到客户端的消息', data)
 
-    //延时返回数据
-    setTimeout(() => {
-      socket.emit('response', { data: a });
-      a++;
-    }, 0);
-  });
-});
+        //延时返回数据
+        setTimeout(() => {
+            socket.emit('response', { data: a })
+            a++
+        }, 0)
+    })
+})
 
-io.listen(4000);
+io.listen(4000)
+console.log('服务器已启动')
